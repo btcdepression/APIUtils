6a63b64ae97196c4176e25016f9ebd15ec19aaa8
Oleksii Kukhar1 <oleksii.kukhar@pearson.com>
2022-04-26 14:58:06 +0300

    senGetRequest impl

--- src/main/java/ApiTest.java
+++ src/main/java/ApiTest.java
@@ -1,2 +1,21 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ApiTest {
+    public static String sendGetRequest(String url) {
+        RestAssured.baseURI = url;
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/posts")
+                .then()
+                .extract().response();
+    //assertEquals(200, response.statusCode());
+    return response.asString();
+    }
 }
